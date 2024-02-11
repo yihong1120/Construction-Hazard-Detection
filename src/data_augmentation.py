e6309060b7388bc1fe025b7af2de983e5953b728
yihong1120 <qazsx748596@gmail.com>
2024-02-12 00:35:03 +0800

    Debug: path to labels.txt

--- src/data_augmentation.py
+++ src/data_augmentation.py
@@ -1,4 +1,3 @@
-import os
 from typing import List, Tuple
 import imageio.v3 as imageio
 import imgaug as ia
@@ -9,14 +8,13 @@ from tqdm import tqdm
 import shutil
 import random
 from pathlib import Path
-import glob
 
 class DataAugmentation:
     """ 
     A class to perform data augmentation for image datasets, especially useful for training machine learning models.
     
     Attributes:
-        train_path (str): The path to the training data.
+        train_path (Path): The path to the training data.
         num_augmentations (int): The number of augmentations to perform per image.
         seq (iaa.Sequential): The sequence of augmentations to apply.
     """
@@ -29,7 +27,7 @@ class DataAugmentation:
             train_path (str): The path to the training data.
             num_augmentations (int): The number of augmentations to perform per image.
         """
-        self.train_path = train_path
+        self.train_path = Path(train_path)
         self.num_augmentations = num_augmentations
         self.seq = self._get_augmentation_sequence()
 
@@ -57,12 +55,12 @@ class DataAugmentation:
             iaa.AddToHueAndSaturation((-20, 20)),
         ], random_order=True)
 
-    def augment_image(self, image_path: str):
+    def augment_image(self, image_path: Path):
         """
         Process and augment a single image.
         """
         image = imageio.imread(image_path)
-        label_path = image_path.replace('images', 'labels').replace('.png', '.txt').replace('.jpg', '.txt').replace('.jpeg', '.txt')
+        label_path = image_path.with_suffix('.txt').parent.parent / 'labels' / image_path.with_suffix('.txt').name
         image_shape = image.shape
         bbs = BoundingBoxesOnImage(self.read_label_file(label_path, image_shape), shape=image_shape)
 
@@ -71,13 +69,14 @@ class DataAugmentation:
                 image = image[:, :, :3]
             image_aug, bbs_aug = self.seq(image=image, bounding_boxes=bbs)
 
-            base_filename = os.path.splitext(os.path.basename(image_path))[0]
-            file_extension = os.path.splitext(image_path)[1]
-            aug_image_filename = f"{base_filename}_aug_{i}{file_extension}"
-            aug_label_filename = f"{base_filename}_aug_{i}.txt"
+            # Clip bounding boxes that fall out of the image
+            bbs_aug = bbs_aug.clip_out_of_image()
 
-            image_aug_path = os.path.join(self.train_path, 'images', aug_image_filename)
-            label_aug_path = os.path.join(self.train_path, 'labels', aug_label_filename)
+            aug_image_filename = image_path.stem + f"_aug_{i}" + image_path.suffix
+            aug_label_filename = image_path.stem + f"_aug_{i}.txt"
+
+            image_aug_path = self.train_path / 'images' / aug_image_filename
+            label_aug_path = self.train_path / 'labels' / aug_label_filename
 
             imageio.imwrite(image_aug_path, image_aug)
             self.write_label_file(bbs_aug, label_aug_path, image_aug.shape[1], image_aug.shape[0])
@@ -86,18 +85,15 @@ class DataAugmentation:
         """
         Perform data augmentation on all images in the dataset.
         """
-        file_types = ['*.png', '*.jpg', '*.jpeg']
-        image_paths = []
-        for file_type in file_types:
-            image_paths.extend(glob.glob(os.path.join(self.train_path, 'images', file_type)))
+        image_paths = list(self.train_path.glob('images/*'))
 
         for image_path in tqdm(image_paths):
             self.augment_image(image_path)
 
     @staticmethod
-    def read_label_file(label_path: str, image_shape: Tuple[int, int, int]) -> List[BoundingBox]:
+    def read_label_file(label_path: Path, image_shape: Tuple[int, int, int]) -> List[BoundingBox]:
         bounding_boxes = []
-        if os.path.exists(label_path):
+        if label_path.exists():
             with open(label_path, 'r') as file:
                 for line in file:
                     class_id, x_center, y_center, width, height = map(float, line.split())
@@ -106,10 +102,13 @@ class DataAugmentation:
                     x2 = (x_center + width / 2) * image_shape[1]
                     y2 = (y_center + height / 2) * image_shape[0]
                     bounding_boxes.append(BoundingBox(x1=x1, y1=y1, x2=x2, y2=y2, label=int(class_id)))
+            print(f"Read {len(bounding_boxes)} bounding boxes from {label_path}")
+        else:
+            print(f"Label file {label_path} does not exist")
         return bounding_boxes
 
     @staticmethod
-    def write_label_file(bounding_boxes: BoundingBoxesOnImage, label_path: str, image_width: int, image_height: int):
+    def write_label_file(bounding_boxes: BoundingBoxesOnImage, label_path: Path, image_width: int, image_height: int):
         with open(label_path, 'w') as f:
             for bb in bounding_boxes:
                 x_center = ((bb.x1 + bb.x2) / 2) / image_width
@@ -131,41 +130,41 @@ class DataAugmentation:
         Raises:
             AssertionError: If the number of image files and label files do not match.
         """
-        image_dir = os.path.join(self.train_path, 'images')
-        label_dir = os.path.join(self.train_path, 'labels')
+        image_dir = self.train_path / 'images'
+        label_dir = self.train_path / 'labels'
 
         # Retrieve paths for all image and label files
-        image_paths: List[str] = glob.glob(os.path.join(image_dir, '*'))
-        label_paths: List[str] = glob.glob(os.path.join(label_dir, '*'))
+        image_paths: List[Path] = list(image_dir.glob('*'))
+        label_paths: List[Path] = list(label_dir.glob('*'))
 
         # Ensure the count of images and labels matches
         assert len(image_paths) == len(label_paths), "The counts of image and label files do not match!"
 
         # Shuffle the paths of images and labels together to maintain correspondence
-        combined: List[Tuple[str, str]] = list(zip(image_paths, label_paths))
+        combined: List[Tuple[Path, Path]] = list(zip(image_paths, label_paths))
         random.shuffle(combined)
         image_paths, label_paths = zip(*combined)
 
         # Create temporary directories for shuffled files
-        temp_image_dir: str = os.path.join(self.train_path, 'temp_images')
-        temp_label_dir: str = os.path.join(self.train_path, 'temp_labels')
-        os.makedirs(temp_image_dir, exist_ok=True)
-        os.makedirs(temp_label_dir, exist_ok=True)
+        temp_image_dir: Path = self.train_path / 'temp_images'
+        temp_label_dir: Path = self.train_path / 'temp_labels'
+        temp_image_dir.mkdir(exist_ok=True)
+        temp_label_dir.mkdir(exist_ok=True)
 
         # Move shuffled files to the temporary directories
         for i, (image_path, label_path) in enumerate(zip(image_paths, label_paths)):
-            new_image_path: str = os.path.join(temp_image_dir, f"{i:06d}" + os.path.splitext(image_path)[1])
-            new_label_path: str = os.path.join(temp_label_dir, f"{i:06d}.txt")
-            shutil.move(image_path, new_image_path)
-            shutil.move(label_path, new_label_path)
+            new_image_path: Path = temp_image_dir / (f"{i:06d}" + image_path.suffix)
+            new_label_path: Path = temp_label_dir / (f"{i:06d}.txt")
+            image_path.rename(new_image_path)
+            label_path.rename(new_label_path)
 
         # Remove the original directories
         shutil.rmtree(image_dir)
         shutil.rmtree(label_dir)
 
         # Rename temporary directories to the original names
-        os.rename(temp_image_dir, image_dir)
-        os.rename(temp_label_dir, label_dir)
+        temp_image_dir.rename(image_dir)
+        temp_label_dir.rename(label_dir)
 
         print("Dataset shuffled successfully.")
 
