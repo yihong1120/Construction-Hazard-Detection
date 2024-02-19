c9cc2b4bb7271d7edf00e8c4d4b0383efe6361cb
yihong1120 <qazsx748596@gmail.com>
2024-02-20 00:30:58 +0800

    Add samples

--- src/live_stream_tracking.py
+++ src/live_stream_tracking.py
@@ -83,3 +83,33 @@ if __name__ == '__main__':
     # Release the video capture object and close all OpenCV windows
     cap.release()
     cv2.destroyAllWindows()
+
+"""sample output
+0: 384x640 1 person, 2 cars, 71.5ms
+Speed: 2.0ms preprocess, 71.5ms inference, 1.7ms postprocess per image at shape (1, 3, 384, 640)
+Timestamp: 1708359532.439757
+IDs: tensor([1., 2., 3.])
+Data (xyxy format):
+tensor([8.0062e+02, 6.1461e+02, 1.0255e+03, 7.5427e+02, 1.0000e+00, 8.6110e-01, 2.0000e+00])
+tensor([0.0000e+00, 5.3632e+02, 8.6468e+02, 9.9007e+02, 2.0000e+00, 7.2071e-01, 2.0000e+00])
+tensor([1.2016e+03, 5.6697e+02, 1.2533e+03, 7.1886e+02, 3.0000e+00, 6.8842e-01, 0.0000e+00])
+
+0: 384x640 1 person, 2 cars, 64.8ms
+Speed: 2.3ms preprocess, 64.8ms inference, 0.7ms postprocess per image at shape (1, 3, 384, 640)
+Timestamp: 1708359532.570106
+IDs: tensor([1., 2., 3.])
+Data (xyxy format):
+tensor([7.9620e+02, 6.1552e+02, 1.0252e+03, 7.5522e+02, 1.0000e+00, 8.6668e-01, 2.0000e+00])
+tensor([0.0000e+00, 5.3310e+02, 8.6284e+02, 9.9407e+02, 2.0000e+00, 8.4004e-01, 2.0000e+00])
+tensor([1.2007e+03, 5.6558e+02, 1.2532e+03, 7.1907e+02, 3.0000e+00, 7.0490e-01, 0.0000e+00])
+
+0: 384x640 1 person, 2 cars, 63.8ms
+Speed: 1.8ms preprocess, 63.8ms inference, 0.5ms postprocess per image at shape (1, 3, 384, 640)
+Timestamp: 1708359532.691226
+IDs: tensor([1., 2., 3.])
+Data (xyxy format):
+tensor([7.9333e+02, 6.1611e+02, 1.0238e+03, 7.5568e+02, 1.0000e+00, 8.8850e-01, 2.0000e+00])
+tensor([0.0000e+00, 5.3068e+02, 8.5880e+02, 1.0035e+03, 2.0000e+00, 8.7344e-01, 2.0000e+00])
+tensor([1.1996e+03, 5.6604e+02, 1.2529e+03, 7.1866e+02, 3.0000e+00, 6.8681e-01, 0.0000e+00])
+
+"""
