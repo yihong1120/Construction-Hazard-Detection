03fd09cfc3ff422675cee85ae559dd15e35d1c8d
yihong1120 <qazsx748596@gmail.com>
2024-02-20 20:52:02 +0800

    Set up output format

--- src/live_stream_tracker.py
+++ src/live_stream_tracker.py
@@ -65,10 +65,12 @@ class LiveStreamDetector:
             results = self.model.track(frame, persist=True)
 
             # Yield the detection results, the frame, and the current timestamp
-            if results[0].boxes is not None:  # Check if there are detections
-                yield results[0].boxes.id, results[0].boxes.data, frame, timestamp
+            if results[0].boxes is not None:
+                ids = results[0].boxes.id.numpy().tolist()  # Convert tensor to list
+                datas = results[0].boxes.data.numpy().tolist()  # Convert tensor to list
+                yield ids, datas, frame, timestamp
             else:
-                yield [], [], frame, timestamp  # Yield empty lists if no detections
+                yield [], [], frame, timestamp
 
             # Exit loop if 'q' is pressed
             if cv2.waitKey(1) & 0xFF == ord('q'):
@@ -89,8 +91,7 @@ class LiveStreamDetector:
             print("Timestamp:", datetime.datetime.fromtimestamp(timestamp).strftime('%Y-%m-%d %H:%M:%S'))
             print("IDs:", ids)
             print("Data (xyxy format):")
-            for data in datas:
-                print(data)
+            print(datas)
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='Perform live stream detection and tracking using YOLOv8.')
@@ -112,21 +113,21 @@ example:
 python live_stream_tracking.py --url https://www.youtube.com/watch?v=ZxL5Hm3mIBk
 
 output:
-0: 384x640 2 persons, 1 truck, 69.4ms
-Speed: 1.8ms preprocess, 69.4ms inference, 1.6ms postprocess per image at shape (1, 3, 384, 640)
-Timestamp: 2024-02-20 00:41:04
-IDs: tensor([ 3., 12., 18.])
+0: 384x640 1 person, 2 cars, 57.3ms
+Speed: 1.7ms preprocess, 57.3ms inference, 0.7ms postprocess per image at shape (1, 3, 384, 640)
+Timestamp: 2024-02-20 19:25:13
+IDs: tensor([1., 2., 3.])
 Data (xyxy format):
-tensor([7.0687e+02, 4.4507e+02, 9.7632e+02, 1.0736e+03, 3.0000e+00, 9.1076e-01, 0.0000e+00])
-tensor([4.5513e-01, 4.7177e+02, 6.6203e+02, 1.0714e+03, 1.2000e+01, 7.5853e-01, 7.0000e+00])
-tensor([1.0427e+03, 6.3850e+02, 1.0775e+03, 7.3198e+02, 1.8000e+01, 5.6060e-01, 0.0000e+00])
+tensor([[0.0000e+00, 5.8455e+02, 8.8502e+02, 1.0074e+03, 1.0000e+00, 8.8050e-01, 2.0000e+00],
+        [9.4921e+02, 5.7641e+02, 1.0652e+03, 8.1061e+02, 2.0000e+00, 8.4425e-01, 0.0000e+00],
+        [7.9515e+02, 6.2592e+02, 9.8356e+02, 7.5257e+02, 3.0000e+00, 7.0361e-01, 2.0000e+00]])
 
-0: 384x640 2 persons, 1 truck, 64.6ms
-Speed: 2.4ms preprocess, 64.6ms inference, 0.7ms postprocess per image at shape (1, 3, 384, 640)
-Timestamp: 2024-02-20 00:41:04
-IDs: tensor([ 3., 12., 18.])
+0: 384x640 1 person, 2 cars, 55.3ms
+Speed: 1.4ms preprocess, 55.3ms inference, 0.5ms postprocess per image at shape (1, 3, 384, 640)
+Timestamp: 2024-02-20 19:25:13
+IDs: tensor([1., 2., 3.])
 Data (xyxy format):
-tensor([7.0379e+02, 4.4713e+02, 9.7202e+02, 1.0722e+03, 3.0000e+00, 9.1378e-01, 0.0000e+00])
-tensor([2.0441e+00, 4.7304e+02, 6.5944e+02, 1.0699e+03, 1.2000e+01, 7.7075e-01, 7.0000e+00])
-tensor([1.0424e+03, 6.3745e+02, 1.0777e+03, 7.3174e+02, 1.8000e+01, 5.5860e-01, 0.0000e+00])
+tensor([[0.0000e+00, 5.8485e+02, 8.8459e+02, 1.0119e+03, 1.0000e+00, 8.7216e-01, 2.0000e+00],
+        [9.4962e+02, 5.7691e+02, 1.0664e+03, 8.1135e+02, 2.0000e+00, 8.4799e-01, 0.0000e+00],
+        [7.9445e+02, 6.2432e+02, 9.8249e+02, 7.5349e+02, 3.0000e+00, 6.3214e-01, 2.0000e+00]])
 """
