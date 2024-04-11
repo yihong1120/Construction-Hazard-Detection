e57453a7ee01fa43eb8d313c7a73c164dae09212
yihong1120 <qazsx748596@gmail.com>
2024-04-11 21:19:41 +0800

    Optimise usage of memory

--- main.py
+++ main.py
@@ -2,9 +2,11 @@ import argparse
 from datetime import datetime
 import json
 import multiprocessing
+from multiprocessing import Pool
+import gc
 import time
 from typing import NoReturn, Dict
-
+import objgraph
 from src.line_notifier import LineNotifier
 from src.monitor_logger import LoggerConfig
 from src.live_stream_detection import LiveStreamDetector
@@ -35,6 +37,7 @@ def main(logger, video_url: str, model_path: str, image_path: str = 'prediction_
 
     # Use the generator function to process detections
     for datas, frame, timestamp in live_stream_detector.generate_detections():
+        objgraph.show_most_common_types(limit=10)
         # Convert UNIX timestamp to datetime object and format it as string
         detection_time = datetime.fromtimestamp(timestamp).strftime('%Y-%m-%d %H:%M:%S')
         print(detection_time)
@@ -65,13 +68,23 @@ def main(logger, video_url: str, model_path: str, image_path: str = 'prediction_
 
             # Update the last_notification_time to the current time
             last_notification_time = timestamp
-
+        objgraph.show_growth(limit=10)
     # Release resources after processing
     live_stream_detector.release_resources()
+
+    gc.collect()
 
 def process_stream(config: Dict[str, str], output_path: str = None) -> NoReturn:
     '''
     Process a single video stream with the given configuration.
+
+    Args:
+        config (Dict[str, str]): Configuration dictionary containing the following keys:
+            - video_url (str): The URL of the live stream to monitor.
+            - model_path (str): The file path of the YOLOv8 model to use for detection.
+            - image_path (str, optional): The file path of the image to send with notifications. Defaults to 'demo_data/prediction_visual.png'.
+            - line_token (str, optional): The LINE notification token. If not provided, notifications are not sent.
+        output_path (str, optional): The file path where output images should be saved. If not specified, images are not saved.
     '''
     logger_config = LoggerConfig()
     logger = logger_config.get_logger()
@@ -89,14 +102,14 @@ def run_multiple_streams(config_file: str, output_path: str = None):
     with open(config_file, 'r') as f:
         configurations = json.load(f)
 
-    processes = []
-    for config in configurations:
-        p = multiprocessing.Process(target=process_stream, args=(config, output_path))
-        p.start()
-        processes.append(p)
+    # For example, use the number of CPU cores available but leave some for other tasks if necessary
+    num_processes = min(len(configurations), multiprocessing.cpu_count() - 1)
 
-    for p in processes:
-        p.join()
+    # Create a multiprocessing pool
+    with Pool(processes=num_processes) as pool:
+        # Map each configuration to the process_stream function
+        # This will automatically distribute the configurations across the pool of processes
+        pool.starmap(process_stream, [(config, output_path) for config in configurations])
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='Run hazard detection on multiple video streams.')
