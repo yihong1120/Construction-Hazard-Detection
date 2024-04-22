e9bcf980381c3bf4e000b6b6ae6adc619438578e
yihong1120 <qazsx748596@gmail.com>
2024-04-23 00:00:50 +0800

    Automatically renew JWT_SECRET_KEY per month

--- examples/Model-Server/app.py
+++ examples/Model-Server/app.py
@@ -4,12 +4,13 @@ from .config import Config
 from .models import db, User
 from .auth import auth_blueprint
 from .detection import detection_blueprint
-from dotenv import load_dotenv
-import os
-load_dotenv()
+from .security import update_secret_key
+from apscheduler.schedulers.background import BackgroundScheduler
+import secrets
+import atexit
 
 app = Flask(__name__)
-app.config['JWT_SECRET_KEY'] = os.getenv('JWT_SECRET_KEY', 'your_fallback_secret_key')
+app.config['JWT_SECRET_KEY'] = secrets.token_urlsafe(16)
 app.config.from_object(Config)
 
 jwt = JWTManager(app)
@@ -17,6 +18,13 @@ jwt = JWTManager(app)
 db.init_app(app)
 app.register_blueprint(auth_blueprint)
 app.register_blueprint(detection_blueprint)
+
+scheduler = BackgroundScheduler()
+scheduler.add_job(func=lambda: update_secret_key(app), trigger="interval", days=30)
+scheduler.start()
+
+# Shut down the scheduler when exiting the app
+atexit.register(lambda: scheduler.shutdown())
 
 if __name__ == '__main__':
     with app.app_context():
