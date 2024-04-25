cae53a24e45bdb4a5ec170ddeb868fb535afbaae
yihong1120 <qazsx748596@gmail.com>
2024-04-25 20:36:20 +0800

    Set up for deployment

--- examples/Stream-Web/app.py
+++ examples/Stream-Web/app.py
@@ -54,7 +54,7 @@ def camera(camera_id: str) -> str:
 
 if __name__ == '__main__':
     # Run the Flask application on localhost at port 8000.
-    app.run(thread=True,host='127.0.0.1', port=8000)
+    app.run(thread=True,host='127.0.0.1', port=8000, debug=False)
 
 """ example usage
 gunicorn -w 4 -b 127.0.0.1:8000 "examples.Stream-Web.app:app"
