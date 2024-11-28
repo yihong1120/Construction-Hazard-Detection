ae4e416490ead0d32025d7d9241db80a8ed28c62
yihong1120 <qazsx748596@gmail.com>
2024-11-28 20:45:41 +0800

    Add deployment files for streaming web frontend

---
+++ examples/streaming_web/frontend/node_modules/rollup/dist/es/parseAst.js
@@ -0,0 +1,12 @@
+/*
+  @license
+	Rollup.js v4.27.3
+	Mon, 18 Nov 2024 16:39:05 GMT - commit 7c0b1f8810013b5a351a976df30a6a5da4fa164b
+
+	https://github.com/rollup/rollup
+
+	Released under the MIT License.
+*/
+import '../native.js';
+export { parseAst, parseAstAsync } from './shared/parseAst.js';
+import 'node:path';
