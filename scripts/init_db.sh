8c2708092677ac05d0b1fcf7b3ed1097c15855cb
yihong1120 <qazsx748596@gmail.com>
2024-11-30 00:19:36 +0800

    Add deployment docker files

--- scripts/init_db.sh
+++ scripts/init_db.sh
@@ -1,19 +1,14 @@
 #!/bin/bash
-# This script will run when the MySQL container starts
 
-# Wait for MySQL to start
-sleep 10
+# Wait for the MySQL container to start and become available
+echo "Waiting for MySQL to be ready..."
+while ! mysqladmin ping -h mysql --silent -u"$MYSQL_USER" -p"$MYSQL_PASSWORD"; do
+    sleep 1  # Pause for one second before checking again
+done
 
-# Run SQL commands to create the table and insert the user
-mysql -h db -u user -ppasscode construction_hazard_detection <<-EOSQL
-    CREATE TABLE IF NOT EXISTS user (
-        id INT AUTO_INCREMENT PRIMARY KEY,
-        username VARCHAR(80) UNIQUE NOT NULL,
-        password_hash VARCHAR(120) NOT NULL
-    );
+# Execute the SQL script to create tables in the database
+echo "Creating tables..."
+mysql -u"$MYSQL_USER" -p"$MYSQL_PASSWORD" -h mysql construction_hazard_detection < /app/init.sql
 
-    INSERT INTO user (username, password_hash) VALUES
-    ('user', 'scrypt:32768:8:1\$2Oy6iiKWmJiNLpbG\$9355a9329c805bf5b7252fe3062b8cc69643febf36fda93bf78ef1a1243a3879fe325db6a801c4d828068fea0f4cfa46791fa0c991cac2433bc1045e0c8c2677')
-    ON DUPLICATE KEY UPDATE
-    password_hash=VALUES(password_hash);
-EOSQL
+# Confirm that the tables were successfully created
+echo "Tables created successfully."
